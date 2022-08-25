f2b024c3b37856836b47ee95efc074651bf75553
AndreiLSevero <110571923+AndreiLSevero@users.noreply.github.com>
2022-08-24 22:14:49 -0300

    Add files via upload
    funções_javascript

---
+++ alfadevweb2/4_estrutura_programacao/13_switch_case/script.js
@@ -0,0 +1,11 @@
+nome = "Denicreidsson";
+switch(nome) {
+    case "Daniel":
+        console.log("Bem vindo Daniel");
+        break;
+    case "João":
+        console.log("Bem vindo João");
+        break;
+    default:
+    console.log("Nome não encontrado!")
+}
