9d0e938a0d23587b899f979d2a0bc6e732bf9150
AndreiLSevero <110571923+AndreiLSevero@users.noreply.github.com>
2022-09-14 22:21:54 -0300

    Delete alfadevweb2/alfadevweb2 directory

--- alfadevweb2/alfadevweb2/3_ex_tipos_de_dado/2_ex_number/script.js
+++
@@ -1,4 +0,0 @@
-/*Escreva três valores em number com números com casa decimal e por operação aritmética;*/
-console.log(8);
-console.log(8.5);
-console.log(80 - 50);
--- alfadevweb2/alfadevweb2/4_estrutura_programacao/2_palavras_reservadas/script.js
+++
@@ -1,6 +0,0 @@
-//3nome='andrei';
-nome3 = 'andrei';
-$sobrenome = 'lopes';
-_idade = 18;
-//!nome = 'teste';
-console.log(nome3 + $sobrenome + _idade);
